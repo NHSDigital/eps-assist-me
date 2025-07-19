be7152f4a49861891e2416a1b27ad6223fb03a0d
Kris Szlapa <kris.szlapa1@nhs.net>
2025-07-19 01:06:44 +0000

    Add metadata to lambda so they don't get flagged as failing cfn-guard

--- packages/cdk/bin/EpsAssistMeApp.ts
+++ packages/cdk/bin/EpsAssistMeApp.ts
@@ -2,6 +2,7 @@
 import {App, Aspects, Tags} from "aws-cdk-lib"
 import {AwsSolutionsChecks} from "cdk-nag"
 import {EpsAssistMeStack} from "../stacks/EpsAssistMeStack"
+import {addCfnGuardMetadata} from "./utils/appUtils"
 
 const app = new App()
 
@@ -25,7 +26,7 @@ Tags.of(app).add("stackName", stackName)
 Tags.of(app).add("version", version)
 Tags.of(app).add("commit", commit)
 
-new EpsAssistMeStack(app, "EpsAssistMeStack", {
+const EpsAssistMe = new EpsAssistMeStack(app, "EpsAssistMeStack", {
   env: {
     region: "eu-west-2",
     account: accountId
@@ -33,4 +34,18 @@ new EpsAssistMeStack(app, "EpsAssistMeStack", {
   stackName: stackName,
   version: version,
   commitId: commit
+})
+
+// Run a synth to add cross region lambdas and roles
+app.synth()
+
+// Add metadata to lambda so they don't get flagged as failing cfn-guard
+addCfnGuardMetadata(EpsAssistMe, "AWS679f53fac002430cb0da5b7982bd2287", "Resource")
+addCfnGuardMetadata(EpsAssistMe, "EpsAssistAccessLogsBucket", "Resource",
+  ["S3_BUCKET_LOGGING_ENABLED", "S3_BUCKET_SSL_REQUESTS_ONLY"]
+)
+
+// Finally run synth again with force to include the added metadata
+app.synth({
+  force: true
 })
