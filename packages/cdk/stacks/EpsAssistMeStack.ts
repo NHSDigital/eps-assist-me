732763d5c206b88bc66c8879a0426d690523de7f
Kris Szlapa <kris.szlapa1@nhs.net>
2025-07-28 11:26:56 +0000

    Update dependencies comment

--- packages/cdk/stacks/EpsAssistMeStack.ts
+++ packages/cdk/stacks/EpsAssistMeStack.ts
@@ -115,7 +115,7 @@ export class EpsAssistMeStack extends Stack {
       endpoint
     })
 
-    // Add a dependency for bedrock kb on the custom resource. Enables vector index to be created before KB
+    // Ensure dependencies are created before knowledge base
     vectorKB.knowledgeBase.node.addDependency(vectorIndex.vectorIndex)
     vectorKB.knowledgeBase.node.addDependency(functions.functions.createIndex)
     vectorKB.knowledgeBase.node.addDependency(openSearchResources.collection.collection)
