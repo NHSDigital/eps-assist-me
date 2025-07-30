696a644c6427cc79772b5b0037d45e8553aa4b67
Kris Szlapa <kris.szlapa1@nhs.net>
2025-07-30 19:24:04 +0000

    Add exact resource ID for StorageDocsBucket

--- packages/cdk/nagSuppressions.ts
+++ packages/cdk/nagSuppressions.ts
@@ -95,8 +95,8 @@ export const nagSuppressions = (stack: Stack) => {
         id: "AwsSolutions-IAM5",
         reason: "Bedrock Knowledge Base requires these permissions to access S3 documents and OpenSearch collection.",
         appliesTo: [
-          "Resource::<StorageDocsBucket*Docs*.Arn>/*",
           "Action::bedrock:Delete*",
+          "Resource::<StorageDocsBucketepsampr16Docs240CC945.Arn>/*",
           "Resource::arn:aws:bedrock:eu-west-2:undefined:knowledge-base/*",
           "Resource::arn:aws:bedrock:eu-west-2:591291862413:knowledge-base/*",
           "Resource::arn:aws:aoss:eu-west-2:undefined:collection/*",
