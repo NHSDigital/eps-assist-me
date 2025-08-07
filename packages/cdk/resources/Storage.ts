f1a1d9d35796d95ea1583ce183c75f6bc48cc8f9
Bence Gadanyi <bence.gadanyi1@nhs.net>
2025-08-07 14:01:28 +0000

    feat: creates dynamo table

--- packages/cdk/resources/Storage.ts
+++ packages/cdk/resources/Storage.ts
@@ -1,6 +1,7 @@
 import {Construct} from "constructs"
 import {Key} from "aws-cdk-lib/aws-kms"
 import {S3Bucket} from "../constructs/S3Bucket"
+import {DynamoDbTable} from "../constructs/DynamoDbTable"
 
 export interface StorageProps {
   readonly stackName: string
@@ -9,6 +10,8 @@ export interface StorageProps {
 export class Storage extends Construct {
   public readonly kbDocsBucket: S3Bucket
   public readonly kbDocsKey: Key
+  public readonly conversationTable: DynamoDbTable
+  public readonly conversationKey: Key
 
   constructor(scope: Construct, id: string, props: StorageProps) {
     super(scope, id)
@@ -24,6 +27,18 @@ export class Storage extends Construct {
       bucketName: `${props.stackName}-Docs`,
       kmsKey: this.kbDocsKey,
       versioned: true
+    })
+
+    // create KMS key for conversation table encryption
+    this.conversationKey = new Key(this, "ConversationKey", {
+      enableKeyRotation: true,
+      description: "KMS key for encrypting conversation sessions"
+    })
+
+    // create DynamoDB table for conversation sessions
+    this.conversationTable = new DynamoDbTable(this, "ConversationTable", {
+      tableName: `${props.stackName}-conversations`,
+      kmsKey: this.conversationKey
     })
   }
 }
