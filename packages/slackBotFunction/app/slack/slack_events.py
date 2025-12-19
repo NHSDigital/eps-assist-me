29fa6262b44edfda003baec57c1243cfb39db6fd
Kieran Wilkinson <kieran.wilkinson4@nhs.net>
2025-12-19 14:30:03 +0000

    feat: Add tests back for citations

--- packages/slackBotFunction/app/slack/slack_events.py
+++ packages/slackBotFunction/app/slack/slack_events.py
@@ -279,7 +279,7 @@ def convert_markdown_to_slack(body: str) -> str:
     body = re.sub(r"\*\*([^*]+)\*\*", r"*\1*", body)
 
     # 4. Handle Lists (Handle various bullet points and dashes, inc. unicode support)
-    body = re.sub(r"\s+(?:-|\u2022)(\s*)", r"\n- ", body)
+    body = re.sub(r"[\r\n\s]*[-•–—▪‣◦⁃][ \t]*", r"\n- ", body)
 
     # 5. Convert Markdown Links [text](url) to Slack <url|text>
     body = re.sub(r"\[([^\]]+)\]\(([^\)]+)\)", r"<\2|\1>", body)
