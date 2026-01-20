65d854d2a4eb84dc0bfc261eb55abd360b5863ab
Kieran Wilkinson <kieran.wilkinson4@nhs.net>
2026-01-20 10:36:03 +0000

    feat: Don't run on INT, PRs or if not deploying

--- .github/scripts/find_s3_bucket.sh
+++ .github/scripts/find_s3_bucket.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
 
-# Arguments
-PARTIAL_NAME=$1
-
-if [ -z "$PARTIAL_NAME" ]; then
-  echo "Error: No partial bucket name provided."
-  exit 1
-fi
-
-echo "Searching for bucket containing: $PARTIAL_NAME..."
+echo "Searching for bucket in CloudFormation exports..."
 
 # List buckets and filter using JMESPath
 # We use 'tail -n 1' or 'awk' to ensure we only get one result if multiple match
 BUCKET_NAME=$(aws cloudformation list-exports --query "Exports[?Name=='epsam:kbDocsBucket:Name'].Value" --output text)
+
 
 if [ -z "$BUCKET_NAME" ] || [ "$BUCKET_NAME" == "None" ]; then
   echo "Error: No bucket found matching '$PARTIAL_NAME'"
