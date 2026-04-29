45b548eb4459ffdf7d063a43b443aa65ffe803db
Kieran Wilkinson <244729020+kieran-wilkinson-4@users.noreply.github.com>
2026-04-29 10:46:22 +0000

    fix: Add Slack KMS Keys

--- packages/cdk/resources/Secrets.ts
+++ packages/cdk/resources/Secrets.ts
@@ -2,6 +2,7 @@ import {Construct} from "constructs"
 import {StringParameter} from "aws-cdk-lib/aws-ssm"
 import {Secret} from "aws-cdk-lib/aws-secretsmanager"
 import {SecretWithParameter} from "../constructs/SecretWithParameter"
+import {Key} from "aws-cdk-lib/aws-kms"
 
 export interface SecretsProps {
   readonly stackName: string
@@ -14,16 +15,24 @@ export class Secrets extends Construct {
   public readonly slackBotSigningSecret: Secret
   public readonly slackBotTokenParameter: StringParameter
   public readonly slackSigningSecretParameter: StringParameter
+  public readonly secretsKmsKey: Key
 
   constructor(scope: Construct, id: string, props: SecretsProps) {
     super(scope, id)
+
+    this.secretsKmsKey = new Key(this, "SecretsKmsKey", {
+      alias: `${props.stackName}-secrets-key`,
+      description: "KMS Key for EPS Assist Secrets",
+      enableKeyRotation: true
+    })
 
     // Create Slack bot OAuth token secret and parameter
     const slackBotToken = new SecretWithParameter(this, "SlackBotToken", {
       secretName: `/${props.stackName}/bot-token`,
       parameterName: `/${props.stackName}/bot-token/parameter`,
       description: "Slack Bot OAuth Token for EPS Assist",
-      secretValue: JSON.stringify({token: props.slackBotToken})
+      secretValue: JSON.stringify({token: props.slackBotToken}),
+      encryptionKey: this.secretsKmsKey
     })
 
     // Create Slack signing secret for request verification
@@ -31,7 +40,8 @@ export class Secrets extends Construct {
       secretName: `/${props.stackName}/signing-secret`,
       parameterName: `/${props.stackName}/signing-secret/parameter`,
       description: "Slack Signing Secret",
-      secretValue: JSON.stringify({secret: props.slackSigningSecret})
+      secretValue: JSON.stringify({secret: props.slackSigningSecret}),
+      encryptionKey: this.secretsKmsKey
     })
 
     // Export secrets and parameters for use by other constructs
